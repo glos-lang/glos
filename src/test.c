dbf38c5e759e18693143ec3fb032c685e41207c8
Shoumodip Kar <shoumodipkar@gmail.com>
2025-08-30 21:26:00 +0530

    Parallelize tests

--- src/test.c
+++ src/test.c
@@ -10,9 +10,7 @@ static const char *replace_extension(SV path, SV old, SV new) {
     return temp_sprintf(SVFmt "." SVFmt, SVArg(base), SVArg(new));
 }
 
-static bool yes_or_no_prompt(const char *prompt) {
-    fprintf(stderr, "\n%s (Y/n): ", prompt);
-
+static bool yes_or_no_prompt(void) {
     char buffer[16];
     if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
         return false;
@@ -24,6 +22,52 @@ static bool yes_or_no_prompt(const char *prompt) {
     }
 
     return buffer[0] == '\0' || buffer[0] == 'y' || buffer[0] == 'Y';
+}
+
+typedef struct {
+    char  *data;
+    size_t count;
+    size_t capacity;
+} SB;
+
+static bool read_file_into_arena(FILE *f, SV *out, SB *s, Arena *a) {
+    const size_t start = s->count;
+
+#define READ_CHUNK_SIZE 4096
+    while (true) {
+        da_grow(s, READ_CHUNK_SIZE);
+        const size_t n = fread(s->data + s->count, 1, READ_CHUNK_SIZE, f);
+        s->count += n;
+
+        if (n < READ_CHUNK_SIZE) {
+            if (feof(f)) {
+                break;
+            }
+
+            if (ferror(f)) {
+                s->count = start;
+                return false;
+            }
+        }
+    }
+#undef READ_CHUNK_SIZE
+
+    out->count = s->count - start;
+    out->data = arena_clone(a, s->data + start, out->count);
+
+    s->count = start;
+    return true;
+}
+
+static bool read_path_into_arena(const char *path, SV *out, SB *s, Arena *arena) {
+    FILE *f = fopen(path, "rb");
+    if (!f) {
+        return false;
+    }
+
+    const bool result = read_file_into_arena(f, out, s, arena);
+    fclose(f);
+    return result;
 }
 
 static bool parse_uint_from_sv(SV s, size_t *n) {
@@ -48,48 +92,6 @@ static bool parse_uint_from_sv(SV s, size_t *n) {
 
     *n = result;
     return true;
-}
-
-typedef struct {
-    char  *data;
-    size_t count;
-    size_t capacity;
-} SB;
-
-static bool read_file_into_sb(FILE *f, SB *s) {
-    const size_t count_save = s->count;
-
-#define READ_CHUNK_SIZE 4096
-    while (true) {
-        da_grow(s, READ_CHUNK_SIZE);
-        const size_t n = fread(s->data + s->count, 1, READ_CHUNK_SIZE, f);
-        s->count += n;
-
-        if (n < READ_CHUNK_SIZE) {
-            if (feof(f)) {
-                break;
-            }
-
-            if (ferror(f)) {
-                s->count = count_save;
-                return false;
-            }
-        }
-    }
-#undef READ_CHUNK_SIZE
-
-    return true;
-}
-
-static bool read_path_into_sb(const char *path, SB *s) {
-    FILE *f = fopen(path, "rb");
-    if (!f) {
-        return false;
-    }
-
-    const bool result = read_file_into_sb(f, s);
-    fclose(f);
-    return result;
 }
 
 static size_t parse_uint_value(SV value, const char *label, const char *path, size_t row, SV line) {
@@ -143,27 +145,9 @@ static SV parse_bytes_value(SV value, SV *contents, const char *label, const cha
 }
 
 typedef struct {
-    SB    *sb;
-    size_t start;
-    size_t count;
-} DynSV;
-
-static DynSV sv_to_dyn_sv(SV sv, SB *sb) {
-    DynSV dyn = {0};
-    dyn.sb = sb;
-    dyn.start = sv.data - sb->data;
-    dyn.count = sv.count;
-    return dyn;
-}
-
-static SV dyn_sv_to_sv(DynSV dyn) {
-    return (SV) {.data = dyn.sb->data + dyn.start, .count = dyn.count};
-}
-
-typedef struct {
-    int   exit;
-    DynSV out;
-    DynSV err;
+    int exit;
+    SV  out;
+    SV  err;
 } Test;
 
 static void print_lines_with_indent(FILE *f, SV sv, const char *indent) {
@@ -186,19 +170,14 @@ static void print_lines_with_indent(FILE *f, SV sv, const char *indent) {
     }
 }
 
-static bool compare_tests(Test expected, Test actual) {
+static bool compare_tests(Test expected, Test actual, const char *name) {
     const bool exit_mismatch = expected.exit != actual.exit;
 
-    const SV expected_out = dyn_sv_to_sv(expected.out);
-    const SV expected_err = dyn_sv_to_sv(expected.err);
-    const SV actual_out = dyn_sv_to_sv(actual.out);
-    const SV actual_err = dyn_sv_to_sv(actual.err);
-
-    const bool stdout_mismatch = !sv_eq(expected_out, actual_out);
-    const bool stderr_mismatch = !sv_eq(expected_err, actual_err);
+    const bool stdout_mismatch = !sv_eq(expected.out, actual.out);
+    const bool stderr_mismatch = !sv_eq(expected.err, actual.err);
 
     if (exit_mismatch || stdout_mismatch || stderr_mismatch) {
-        fprintf(stderr, "FAIL\n");
+        fprintf(stderr, "\nERROR: Test case '%s' FAILED\n", name);
 
         if (exit_mismatch) {
             fprintf(stderr, "\n");
@@ -210,31 +189,31 @@ static bool compare_tests(Test expected, Test actual) {
         if (stdout_mismatch) {
             fprintf(stderr, "\n");
             fprintf(stderr, "Standard Output:\n");
-            fprintf(stderr, "  Expected: %zu byte(s)", expected_out.count);
-            print_lines_with_indent(stderr, expected_out, "    ");
+            fprintf(stderr, "  Expected: %zu byte(s)", expected.out.count);
+            print_lines_with_indent(stderr, expected.out, "    ");
 
             fprintf(stderr, "\n");
-            fprintf(stderr, "  Actual:   %zu byte(s)", actual_out.count);
-            print_lines_with_indent(stderr, actual_out, "    ");
+            fprintf(stderr, "  Actual:   %zu byte(s)", actual.out.count);
+            print_lines_with_indent(stderr, actual.out, "    ");
         }
 
         if (stderr_mismatch) {
             fprintf(stderr, "\n");
             fprintf(stderr, "Standard Error:\n");
-            fprintf(stderr, "  Expected: %zu byte(s)", expected_err.count);
-            print_lines_with_indent(stderr, expected_err, "    ");
+            fprintf(stderr, "  Expected: %zu byte(s)", expected.err.count);
+            print_lines_with_indent(stderr, expected.err, "    ");
 
             fprintf(stderr, "\n");
-            fprintf(stderr, "  Actual:   %zu byte(s)", actual_err.count);
-            print_lines_with_indent(stderr, actual_err, "    ");
+            fprintf(stderr, "  Actual:   %zu byte(s)", actual.err.count);
+            print_lines_with_indent(stderr, actual.err, "    ");
         }
         return false;
     } else {
-        fprintf(stderr, "PASS\n");
         return true;
     }
 }
 
+// TODO: CLI flag to control parallel tests count
 static void usage(FILE *f) {
     fprintf(f, "Usage:\n");
     fprintf(f, "    test [FLAGS...]\n");
@@ -255,8 +234,94 @@ static const char *shift(int *argc, char ***argv, const char *expected) {
     return *(*argv)++;
 }
 
+typedef struct {
+    const char *name;
+
+    bool        record_exists;
+    const char *record_path;
+
+    Proc  proc;
+    FILE *pout;
+    FILE *perr;
+
+    Test expected;
+} Unit;
+
+typedef struct {
+    Unit  *data;
+    size_t count;
+    size_t capacity;
+    size_t maximum;
+} Units;
+
+static void flush_units(Units *units, SB *sb, Arena *arena, bool check) {
+    for (size_t i = 0; i < units->count; i++) {
+        Unit it = units->data[i];
+
+        Test actual = {0};
+        actual.exit = cmd_wait(it.proc);
+
+        if (!read_file_into_arena(it.pout, &actual.out, sb, arena)) {
+            fprintf(stderr, "ERROR: Could not read standard output of test case '%s'\n", it.name);
+            exit(1);
+        }
+
+        if (!read_file_into_arena(it.perr, &actual.err, sb, arena)) {
+            fprintf(stderr, "ERROR: Could not read standard error of test case '%s'\n", it.name);
+            exit(1);
+        }
+
+        fclose(it.pout);
+        fclose(it.perr);
+
+        bool need_to_record = false;
+        if (it.record_exists) {
+            if (!compare_tests(it.expected, actual, it.name)) {
+                if (check) {
+                    exit(1);
+                }
+
+                fprintf(stderr, "\nRecord new behavior for test case '%s' (Y/n): ", it.name);
+                if (yes_or_no_prompt()) {
+                    need_to_record = true;
+                }
+            }
+        } else {
+            need_to_record = true;
+        }
+
+        if (need_to_record) {
+            FILE *f = fopen(it.record_path, "wb");
+            if (!f) {
+                fprintf(stderr, "ERROR: Could not write file '%s'\n", it.record_path);
+                exit(1);
+            }
+
+            fprintf(f, "EXIT %d\n", actual.exit);
+
+            fprintf(f, "STDOUT %zu\n", actual.out.count);
+            fwrite(actual.out.data, actual.out.count, 1, f);
+            fprintf(f, "\n");
+
+            fprintf(f, "STDERR %zu\n", actual.err.count);
+            fwrite(actual.err.data, actual.err.count, 1, f);
+            fprintf(f, "\n");
+
+            fclose(f);
+
+            if (!it.record_exists) {
+                fprintf(stderr, "Recorded test case '%s'\n", it.name);
+            }
+        }
+    }
+
+    units->count = 0;
+    arena_free(arena);
+}
+
 int main(int argc, char **argv) {
-    bool check = false;
+    bool  check = false;
+    Units units = {.maximum = 5};
 
     shift(&argc, &argv, "Program name");
     while (argc) {
@@ -278,124 +343,77 @@ int main(int argc, char **argv) {
         }
     }
 
-    Arena arena = {0};
     Paths paths = {0};
-    if (!read_dir(&paths, get_current_dir(&arena), ".", sv_from_cstr("glos"), &arena)) {
+    Arena paths_arena = {0};
+    if (!read_dir(&paths, get_current_dir(&paths_arena), ".", sv_from_cstr("glos"), &paths_arena)) {
         fprintf(stderr, "ERROR: Could not contents of current directory\n");
         exit(1);
     }
 
-    SB  sb = {0};
-    Cmd cmd = {0};
+    SB    sb = {0};
+    Cmd   cmd = {0};
+    Arena arena = {0};
     for (size_t i = 0; i < paths.count; i++) {
         const char *it = paths.data[i];
         const char *record_path = replace_extension(sv_from_cstr(it), sv_from_cstr("glos"), sv_from_cstr("txt"));
 
-        sb.count = 0;
-        const bool record_exists = read_path_into_sb(record_path, &sb);
+        SV         contents = {0};
+        const bool record_exists = read_path_into_arena(record_path, &contents, &sb, &arena);
 
-        Test expected = {0};
-        if (record_exists) {
-            SV contents = {sb.data, sb.count};
-            for (size_t row = 1; contents.count; row++) {
-                SV it = sv_split(&contents, '\n');
-                SV line = sv_trim(it, ' ');
-                SV key = sv_split(&line, ' ');
-                SV value = sv_trim(line, ' ');
-                if (sv_match(key, "EXIT")) {
-                    expected.exit = parse_uint_value(value, "exit code", record_path, row, line);
-                } else if (sv_match(key, "STDOUT")) {
-                    expected.out = sv_to_dyn_sv(
-                        parse_bytes_value(value, &contents, "standard output", record_path, &row, line), &sb);
-                } else if (sv_match(key, "STDERR")) {
-                    expected.err = sv_to_dyn_sv(
-                        parse_bytes_value(value, &contents, "standard error", record_path, &row, line), &sb);
-                } else {
-                    fprintf(stderr, "%s:%zu: ERROR: Invalid key '" SVFmt "'\n", record_path, row, SVArg(key));
-                    exit(1);
-                }
-            }
-
-            fprintf(stderr, "Replaying '%s' ... ", it);
-        } else {
-            // TODO: Prompt the user for the CLI args
-            fprintf(stderr, "Recording '%s' ... ", it);
-        }
-
-        da_push(&cmd, "../glos");
-        da_push(&cmd, "-r");
-        da_push(&cmd, it);
-
-        Test actual = {0};
         {
-            FILE *stdout_pipe = NULL;
-            FILE *stderr_pipe = NULL;
-            actual.exit = cmd_run_sync(&cmd, (CmdStdio) {.out = &stdout_pipe, .err = &stderr_pipe});
-
-            actual.out.sb = &sb;
-            actual.out.start = sb.count;
-            if (!read_file_into_sb(stdout_pipe, &sb)) {
-                fprintf(stderr, "\nERROR: Could not read standard output of process\n");
-                exit(1);
-            }
-            actual.out.count = sb.count - actual.out.start;
-
-            actual.err.sb = &sb;
-            actual.err.start = sb.count;
-            if (!read_file_into_sb(stderr_pipe, &sb)) {
-                fprintf(stderr, "\nERROR: Could not read standard error of process\n");
-                exit(1);
-            }
-            actual.err.count = sb.count - actual.err.start;
-
-            fclose(stdout_pipe);
-            fclose(stderr_pipe);
-        }
-
-        bool need_to_record = false;
-        if (record_exists) {
-            if (!compare_tests(expected, actual)) {
-                if (check) {
-                    exit(1);
+            Test expected = {0};
+            if (record_exists) {
+                for (size_t row = 1; contents.count; row++) {
+                    SV it = sv_split(&contents, '\n');
+                    SV line = sv_trim(it, ' ');
+                    SV key = sv_split(&line, ' ');
+                    SV value = sv_trim(line, ' ');
+                    if (sv_match(key, "EXIT")) {
+                        expected.exit = parse_uint_value(value, "exit code", record_path, row, line);
+                    } else if (sv_match(key, "STDOUT")) {
+                        expected.out = parse_bytes_value(value, &contents, "standard output", record_path, &row, line);
+                    } else if (sv_match(key, "STDERR")) {
+                        expected.err = parse_bytes_value(value, &contents, "standard error", record_path, &row, line);
+                    } else {
+                        fprintf(stderr, "%s:%zu: ERROR: Invalid key '" SVFmt "'\n", record_path, row, SVArg(key));
+                        exit(1);
+                    }
                 }
 
-                if (yes_or_no_prompt("Record new behavior")) {
-                    need_to_record = true;
-                }
-            }
-        } else {
-            need_to_record = true;
-        }
-
-        if (need_to_record) {
-            FILE *f = fopen(record_path, "wb");
-            if (!f) {
-                fprintf(stderr, "\nERROR: Could not write file '%s'\n", record_path);
-                exit(1);
+                fprintf(stderr, "Replaying '%s'\n", it);
+            } else {
+                // TODO: Prompt the user for the CLI args
+                fprintf(stderr, "Recording '%s'\n", it);
             }
 
-            fprintf(f, "EXIT %d\n", actual.exit);
+            da_push(&cmd, "../glos");
+            da_push(&cmd, "-r");
+            da_push(&cmd, it);
 
-            fprintf(f, "STDOUT %zu\n", actual.out.count);
-            const SV actual_out = dyn_sv_to_sv(actual.out);
-            fwrite(actual_out.data, actual_out.count, 1, f);
-            fprintf(f, "\n");
+            Unit unit = {0};
+            unit.name = it;
 
-            fprintf(f, "STDERR %zu\n", actual.err.count);
-            const SV actual_err = dyn_sv_to_sv(actual.err);
-            fwrite(actual_err.data, actual_err.count, 1, f);
-            fprintf(f, "\n");
+            unit.record_exists = record_exists;
+            unit.record_path = record_path;
 
-            fclose(f);
+            unit.proc = cmd_run_async(&cmd, (CmdStdio) {.out = &unit.pout, .err = &unit.perr});
+            unit.expected = expected;
+            da_push(&units, unit);
 
-            if (!record_exists) {
-                fprintf(stderr, "OK\n");
+            if (units.count < units.maximum) {
+                continue;
             }
         }
+
+        flush_units(&units, &sb, &arena, check);
     }
+
+    flush_units(&units, &sb, &arena, check);
 
     da_free(&sb);
     da_free(&cmd);
+    da_free(&units);
+
     da_free(&paths);
-    arena_free(&arena);
+    arena_free(&paths_arena);
 }
