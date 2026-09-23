0b9ed39f581e750ceb427d50bf99d5c17c3ea241
Shoumodip Kar <shoumodipkar@gmail.com>
2026-09-23 13:16:26 +0530

    Add `errno` to the standard library

--- errno.py
+++
@@ -1,5 +0,0 @@
-import errno, os, re
-names = [n for n in dir(errno) if re.fullmatch(r'E[A-Z0-9]+', n)]
-for name in sorted(names, key=lambda n: getattr(errno, n)):
-    v = getattr(errno, name)
-    print(f"{name} \"{os.strerror(v)}\" = {v}")
